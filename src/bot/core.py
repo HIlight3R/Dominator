7c3e0488caf1df2ac03e4d7791913a8ab4202e0c
HIlight3R <hilight3r7@gmail.com>
2020-08-24 16:03:24 +0300

    Обновление 0.2 - Фильтрация чата - Автороли - Уведомления о новых участниках - Отправка личных сообщений при наказании - Улучшены показатели производительности - Исправлены баги, опечатки и недочеты

--- src/bot/core.py
+++ src/bot/core.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 
-from config import PREFIX, TOKEN, DELETE_COMMANDS, MUTE_ROLE_NAME
+from config import PREFIX, TOKEN, DELETE_COMMANDS, MUTE_ROLE_ID, USE_AUTO_ROLE, AUTO_ROLE_ID, USE_NEWCOMER_NOTICE, \
+    NEWCOMER_NOTICE_CHANNEL, SEND_PUNISHMENT_PERSONAL_MESSAGE, USE_CHAT_FILTRATION, BAD_WORD_LIST
 
 # Настройка префикса для команд
 client = commands.Bot(command_prefix=PREFIX)
@@ -10,9 +11,47 @@ client.remove_command('help')
 
 def main():
     @client.event
-    async def on_ready():
+    async def on_connect():
         """Сообщение при подключении"""
-        print('Бот запущен')
+        print('Бот подключен')
+        await client.change_presence(status=discord.Status.online, activity=discord.Game(f'{PREFIX}help'))
+
+    @client.event
+    async def on_member_join(member: discord.Member):
+        if USE_AUTO_ROLE or USE_NEWCOMER_NOTICE:
+            if USE_AUTO_ROLE:
+                role = discord.utils.get(member.guild.roles, id=AUTO_ROLE_ID)
+                await member.add_roles(role)
+            if USE_NEWCOMER_NOTICE:
+                channel = client.get_channel(NEWCOMER_NOTICE_CHANNEL)
+                await channel.send(
+                    embed=discord.Embed(description=f'Пользователь {member.mention} присоединился к серверу.',
+                                        color=0xff8000))
+
+    @client.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send(
+                f'{ctx.author.mention}, такой команды не существует, воспользуйтесь справкой, введя {PREFIX}help.')
+
+    @client.event
+    async def on_message(message: discord.Message):
+        if USE_CHAT_FILTRATION:
+            await client.process_commands(message)
+            msg = message.content.lower()
+            if msg in BAD_WORD_LIST:
+                await message.delete()
+                await message.channel.send(f'{message.author.mention}, не надо так писать!')
+
+    @client.event
+    async def on_resumed():
+        """Сообщение при повторном подключении"""
+        print('Бот снова подключен')
+
+    @client.event
+    async def on_disconnect():
+        """Сообщение при отключении"""
+        print('Бот отключен')
 
     @client.command(name="ping")
     async def _ping(ctx):
@@ -43,6 +82,11 @@ def main():
                               'без причины.')
         embed.add_field(name=f'{PREFIX}unban ID_ПОЛЬЗОВАТЕЛЯ',
                         value='Разблокирует участника с ID ID_ПОЛЬЗОВАТЕЛЯ на сервере, если он был заблокирован.')
+        embed.add_field(name=f'{PREFIX}mute ПОЛЬЗОВАТЕЛЬ',
+                        value='Навсегда запрещает участнику ПОЛЬЗОВАТЕЛЬ писать сообщения в текстовых каналах.')
+        embed.add_field(name=f'{PREFIX}unmute ПОЛЬЗОВАТЕЛЬ',
+                        value='Разрешает участнику ПОЛЬЗОВАТЕЛЬ писать сообщения в текстовые каналы, если до этого он '
+                              'не мог это делать.')
         embed.set_thumbnail(url=client.user.avatar_url)
         embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
@@ -51,70 +95,97 @@ def main():
     @commands.has_permissions(administrator=True)
     async def _clear(ctx, amount=None):
         """Очистка чата"""
+        if DELETE_COMMANDS:
+            await ctx.message.delete()
         if amount:
-            await ctx.channel.purge(limit=amount + 1)
+            await ctx.channel.purge(limit=amount)
         else:
             await ctx.channel.purge()
 
     @client.command(name='kick')
     @commands.has_permissions(administrator=True)
     async def _kick(ctx, member: discord.Member, *, reason: str = None):
-        """Выгнать участника"""
+        """Кикнуть участника"""
         if DELETE_COMMANDS:
             await ctx.message.delete()
         await member.kick(reason=reason)
         if reason:
             await ctx.send(f'Участник {member.mention} выгнан с сервера.\nПричина: "{reason}".')
+            if SEND_PUNISHMENT_PERSONAL_MESSAGE:
+                await member.send(f'Вы были выгнаны с сервера по причине: "{reason}".')
         else:
             await ctx.send(f'Участник {member.mention} выгнан с сервера без причины.')
+            if SEND_PUNISHMENT_PERSONAL_MESSAGE:
+                await member.send('Вы были выгнаны с сервера без причины.')
 
     @client.command(name='ban')
     @commands.has_permissions(administrator=True)
     async def _ban(ctx, member: discord.Member, *, reason: str = None):
-        """Выгнать и заблокировать участника"""
+        """Забанить участника"""
         if DELETE_COMMANDS:
             await ctx.message.delete()
         await member.ban(reason=reason)
         if reason:
             await ctx.send(f'Участник {member.mention} забанен на сервере.\nПричина: "{reason}".')
+            if SEND_PUNISHMENT_PERSONAL_MESSAGE:
+                await member.send(f'Вы были забанены на сервере по причине: "{reason}".')
         else:
             await ctx.send(f'Участник {member.mention} забанен на сервере без причины.')
+            if SEND_PUNISHMENT_PERSONAL_MESSAGE:
+                await member.send('Вы были забанены на сервере без причины.')
 
     @client.command(name='unban')
     @commands.has_permissions(administrator=True)
     async def _unban(ctx, *, user_id: int):
-        """Разблокировать участника"""
+        """Разбанить участника"""
         await ctx.message.delete()
         try:
             user = await client.fetch_user(user_id=user_id)
             await ctx.guild.unban(user)
             await ctx.send(f'Участник с ID {user_id} успешно разбанен.')
-        except:
+            if SEND_PUNISHMENT_PERSONAL_MESSAGE:
+                await user.send('Вы были разбанены на сервере.')
+        except discord.DiscordException:
             await ctx.send(f'Участник с ID {user_id} не забанен, поэтому не может быть разбанен.')
 
     @client.command(name='mute')
     @commands.has_permissions(administrator=True)
     async def _mute(ctx, member: discord.Member):
-        """Запретить писать участнику в чат"""
+        """Замьютить участника"""
         if DELETE_COMMANDS:
             await ctx.message.delete()
-        mute_role = discord.utils.get(ctx.message.guild.roles, name=MUTE_ROLE_NAME)
+        mute_role = discord.utils.get(ctx.message.guild.roles, id=MUTE_ROLE_ID)
         await member.add_roles(mute_role)
         await ctx.send(f'Участник {member.mention} теперь может только читать сообщения.')
+        if SEND_PUNISHMENT_PERSONAL_MESSAGE:
+            await member.send('Вы были замьючены на сервере.')
 
     @client.command(name='unmute')
     @commands.has_permissions(administrator=True)
     async def _unmute(ctx, member: discord.Member):
-        """Разрешить писать участнику в чат"""
+        """Размьютить участника"""
         if DELETE_COMMANDS:
             await ctx.message.delete()
-        mute_role = discord.utils.get(ctx.message.guild.roles, name=MUTE_ROLE_NAME)
+        mute_role = discord.utils.get(ctx.message.guild.roles, id=MUTE_ROLE_ID)
         if mute_role in member.roles:
             await member.remove_roles(mute_role)
             await ctx.send(f'Участник {member.mention} снова может писать в чат.')
+            if SEND_PUNISHMENT_PERSONAL_MESSAGE:
+                await member.send('Вы были размьючены на сервере.')
         else:
-            await ctx.send(f'Участник {member.mention} не в муте, поэтому не может быть размучен.')
+            await ctx.send(f'Участник {member.mention} не в мьюте, поэтому не может быть размьючен.')
+
+    @_kick.error
+    @_ban.error
+    @_unban.error
+    @_mute.error
+    @_unmute.error
+    async def moderation_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(
+                f'{ctx.author.mention}, вы неверно ввели аргументы, воспользуйтесь командой {PREFIX}help для справки.')
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f'{ctx.author.mention}, у вас недостаточно прав для выполнения данной команды.')
 
     # Запуск бота
     client.run(TOKEN)
-    print('Бот остановлен')
